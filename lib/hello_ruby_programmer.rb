e6b5a9ce52749cc035a9222086e973639afe3bb0
Samuel Cornet <samuel@noukod.com>
2018-08-29 20:07:00 +0000

    automatically backed up by learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,5 +1,5 @@
 def greeting(name)
-    puts "Hello #{name}. It's nice to meet you."
+    puts "Hi #{name}. Welcome to the wonderful world of Ruby programming."
     name = gets.strip
     greeting(name)
 end
